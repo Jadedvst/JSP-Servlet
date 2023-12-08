f19d9e9014b4a5e2a55644134190d7c0460bba94
pnusw14 <dustofjade@gmail.com>
2023-12-08 18:02:50 +0900

    23.12.08

--- ServletStudy/src/main/java/edu/pnu/servlet/MyServlet02.java
+++ ServletStudy/src/main/java/edu/pnu/servlet/MyServlet02.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/myservlet02") //url 호출
+@WebServlet("/myservlet02") //url 호출, web.xml 불필요
 public class MyServlet02 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
@@ -17,7 +17,9 @@ public class MyServlet02 extends HttpServlet {
 	protected void service(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException {
 		System.out.println("MyServlet02 입니다");
+		
 		res.setContentType("text/plain; charset=utf-8");
+		//한글이 깨지지 않게
 		PrintWriter out = res.getWriter();
 		out.println("이것이 서블릿에서 출력하는 문서입니다.");
 		out.println("보이면 성공입니다!!");
